6785048f64c06090ddebcd9a766ad50aee24f603
Anas Khan <anasify404@gmail.com>
2026-08-25 15:31:53 +0530

    Add player and legend data fetching functionality

---
+++ scripts.js
@@ -0,0 +1,221 @@
+const form = document.querySelector("#input");
+const inputId = document.querySelector("#inputId");
+const playerNotFound = document.querySelector("#playerNotFound");
+const playerIdSection = document.querySelector("#playerIdSection");
+const playerTopLegends = document.querySelector("#playerTopLegends");
+const playerHeading = document.querySelector("#playerHeading");
+const legendHeading = document.querySelector("#legendHeading");
+
+// playerIdSection.innerHTML = "";
+
+function getLegendProfiles() {
+  fetch(`https://api.brawlhalla.com/v1/static/legends`)
+    .then((resp) => {
+      return resp.json();
+    })
+    .then((data) => {
+      console.log(data);
+      let legendProfiles1 = data.legends;
+
+      // legendProfiles.legend_name = data.legend_name;
+      console.log(legendProfiles);
+    })
+    .catch((err) => {
+      console.log("404");
+    });
+  fetch(`https://api.brawlhalla.com/v1/static/legends?page=2`)
+    .then((resp) => {
+      return resp.json();
+    })
+    .then((data) => {
+      console.log(data);
+      let legendProfiles2 = data.legends;
+
+      // legendProfiles.legend_name = data.legend_name;
+      console.log(legendProfiles2);
+    })
+    .catch((err) => {
+      console.log("404");
+    });
+}
+
+function getData(id) {
+  fetch(`https://api.brawlhalla.com/v1/player/stats?brawlhalla_id=${id}`)
+    .then((resp) => {
+      return resp.json();
+    })
+    .then((data) => {
+      let playerData = {};
+      playerData.brawlhalla_id = data.brawlhalla_id;
+      playerData.name = data.name;
+      playerData.games = data.games;
+      playerData.wins = data.wins;
+      playerData.losses = data.games - data.wins;
+      playerData.winRate = ((data.wins / data.games) * 100).toFixed(2);
+      // console.log(playerData);
+      let legendsData = data.legends.slice(0, 5);
+      // console.log(legendsData);
+      showCard(playerData, legendsData);
+    })
+    .catch((err) => {
+      (consolv, e.log(err));
+      if (playerNotFound.classList.contains("toggle")) {
+        playerNotFound.classList.remove("toggle");
+      }
+      playerIdSection.innerHTML = "";
+      playerTopLegends.innerHTML = "";
+    });
+}
+
+function createPlayerCard(obj) {
+  const playerCard = document.createElement("div");
+  playerCard.classList.add("playerCard");
+
+  const playerContent = document.createElement("div");
+  playerContent.classList.add("playerContent");
+
+  const name = document.createElement("h3");
+  name.classList.add("name");
+  name.textContent = `@${obj.name}`;
+
+  const bhId = document.createElement("p");
+  bhId.classList.add("bhId");
+  bhId.textContent = `ID: ${obj.brawlhalla_id}`;
+
+  playerContent.append(name, bhId);
+
+  const stats = document.createElement("div");
+  stats.classList.add("stats");
+
+  const section1 = document.createElement("div");
+  section1.classList.add("section1");
+
+  // const level = document.createElement("p");
+  // level.textContent = `Level: ${obj.level}`;
+
+  // const xp = document.createElement("p");
+  // xp.textContent = `XP: ${obj.xp}`;
+
+  const games = document.createElement("p");
+  games.textContent = `Games: ${obj.games}`;
+
+  const section2 = document.createElement("div");
+  section2.classList.add("section2");
+
+  const wins = document.createElement("p");
+  wins.textContent = `Wins: ${obj.wins}`;
+
+  const losses = document.createElement("p");
+  losses.textContent = `Loss: ${obj.games - obj.wins}`;
+
+  const winRate = document.createElement("p");
+  const rate = ((obj.wins / obj.games) * 100).toFixed(2);
+  winRate.textContent = `Win Rate: ${rate}%`;
+
+  section1.append(games, winRate);
+  section2.append(wins, losses);
+
+  stats.append(section1, section2);
+
+  const winRateGraph = document.createElement("div");
+  winRateGraph.classList.add("winRateGraph");
+
+  const win = document.createElement("div");
+  win.classList.add("win");
+  win.style.width = `${rate}%`;
+
+  const loss = document.createElement("div");
+  loss.classList.add("loss");
+  loss.style.width = `${100 - rate}%`;
+
+  winRateGraph.append(win, loss);
+
+  playerCard.append(playerContent, stats, winRateGraph);
+  playerIdSection.append(playerCard);
+}
+
+function createLegendCard(obj) {
+  const legendCard = document.createElement("div");
+  legendCard.classList.add("legendCard");
+
+  const legendPfp = document.createElement("img");
+  legendPfp.classList.add("legendPfp");
+  legendPfp.src = "https://placehold.co/300x300";
+  legendPfp.alt = obj.legend_id;
+
+  const legendInfo = document.createElement("div");
+  legendInfo.classList.add("legend-info");
+
+  const legendName = document.createElement("h3");
+  legendName.classList.add("legend-name");
+  legendName.textContent = `Legend: ${obj.legend_name}`;
+
+  const legendStats = document.createElement("div");
+  legendStats.classList.add("legend-stats");
+
+  const games = document.createElement("p");
+  games.textContent = `Games: ${obj.games}`;
+
+  const wins = document.createElement("p");
+  wins.textContent = `Wins: ${obj.wins}`;
+
+  const winRate = document.createElement("p");
+  winRate.textContent = `Win Rate: ${((obj.wins / obj.games) * 100).toFixed(2)}`;
+
+  const kos = document.createElement("p");
+  kos.textContent = `KOs: ${obj.kos}`;
+
+  const spearKOs = document.createElement("p");
+  spearKOs.textContent = `weapon_one KOs:${obj.ko_weapon_one}`;
+
+  const lanceKOs = document.createElement("p");
+  lanceKOs.textContent = `weapon_two KOs:${obj.ko_weapon_two}`;
+
+  const falls = document.createElement("p");
+  falls.textContent = `Falls: ${obj.falls}`;
+
+  const suicides = document.createElement("p");
+  suicides.textContent = `Suicides: ${obj.suicides}`;
+
+  legendStats.append(
+    games,
+    wins,
+    winRate,
+    kos,
+    spearKOs,
+    lanceKOs,
+    falls,
+    suicides,
+  );
+
+  legendInfo.append(legendName, legendStats);
+  legendCard.append(legendPfp, legendInfo);
+  playerTopLegends.append(legendCard);
+}
+//pd - playerdata and ld is legendsdata
+function showCard(pd, ld) {
+  playerIdSection.innerHTML = "";
+  playerTopLegends.innerHTML = "";
+
+  if (
+    playerHeading.classList.contains("toggle") &&
+    legendHeading.classList.contains("toggle")
+  ) {
+    playerHeading.classList.remove("toggle");
+    legendHeading.classList.remove("toggle");
+  }
+  createPlayerCard(pd);
+
+  for (let i = 0; i < ld.length; i++) {
+    createLegendCard(ld[i]);
+  }
+  if (playerNotFound.classList.contains("toggle")) {
+    playerNotFound.classList.add("toggle");
+  }
+}
+
+form.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  getLegendProfiles();
+  getData(inputId.value);
+});
